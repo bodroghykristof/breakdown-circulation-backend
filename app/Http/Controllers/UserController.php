69917aa026d786a98a5bc63a144b64f8df383e7a
reClicked <gamper.renato@gmail.com>
2021-01-12 11:09:29 +0100

    Create UserController for registration

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,9 +2,43 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class UserController extends Controller
 {
-    //
+    public function register(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            "name" => "required",
+            "email" => "required|email",
+            "password" => "required"
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                "status" => 400,
+                "message" => "Bad request",
+                "validation_errors" => $validator->errors()
+            ]);
+        }
+
+        $userData = $request->all();
+        $userData["password"] = bcrypt($userData['password']);
+        $user = User::query()->create($userData);
+
+        if (!is_null($user)) {
+            return response()->json([
+                "status" => 200,
+                "success" => true,
+                "message" => "User created successfully",
+                "data" => $user]);
+        } else {
+            return response()->json([
+                "status" => "failed",
+                "success" => false,
+                "message" => "Whoops! Failed to register. Please try again."]);
+        }
+    }
 }
