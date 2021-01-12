91c15eea0368acfb93a87fb8639bdfde2f351da6
bodroghykristof <bodroghykristof@gmail.com>
2021-01-12 12:25:01 +0100

    Handle different scenarios using status codes and error messages for registration

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
+use Illuminate\Database\QueryException;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 
@@ -21,24 +22,42 @@ class UserController extends Controller
                 "status" => 400,
                 "message" => "Bad request",
                 "validation_errors" => $validator->errors()
-            ]);
+            ], 400);
         }
 
         $userData = $request->all();
         $userData["password"] = bcrypt($userData['password']);
-        $user = User::query()->create($userData);
+
+        try {
+            $user = User::query()->create($userData);
+        }
+        catch (QueryException $e) {
+            $errorCode = $e->errorInfo[1];
+            if ($errorCode == 1062){
+                return response()->json([
+                    "status" => 409,
+                    "success" => false,
+                    "message" => "Email already used"
+                ], 409);
+            }
+            return response()->json([
+                "status" => 500,
+                "success" => false,
+                "message" => "Could not register. Server Error."
+            ], 500);
+        }
 
         if (!is_null($user)) {
             return response()->json([
                 "status" => 200,
                 "success" => true,
                 "message" => "User created successfully",
-                "data" => $user]);
+                "data" => $user], 200);
         } else {
             return response()->json([
                 "status" => "failed",
                 "success" => false,
-                "message" => "Whoops! Failed to register. Please try again."]);
+                "message" => "Whoops! Failed to register. Please try again."], 400);
         }
     }
 }
