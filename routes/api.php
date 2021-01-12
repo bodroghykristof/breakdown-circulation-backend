199bf00295bb2cc330c8eaec3fc5b07e7eea5673
reClicked <gamper.renato@gmail.com>
2021-01-12 14:50:55 +0100

    Add route for logout

--- routes/api.php
+++ routes/api.php
@@ -21,3 +21,6 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 
 Route::post("/register", [UserController::class, "register"]);
 Route::post("/login", [UserController::class, "login"]);
+Route::group(["middleware" => ["auth:sanctum"]], function() {
+    Route::post("/logout", [UserController::class, "logout"]);
+});
