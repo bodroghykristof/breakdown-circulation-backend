23d22f85cc2c656b81bac4a11780cea155955a98
CocoBloom <virag.baranyai2@gmail.com>
2021-02-16 12:56:45 +0100

    add getAll() to /get-all-cocktails endpoint

--- routes/api.php
+++ routes/api.php
@@ -21,10 +21,14 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get("/get-all-cocktails", [CocktailController::class, "getAllCocktails"]);
+Route::get("/get-all-cocktails", [CocktailController::class, "getAll"]);
+
+
 Route::post("/register", [UserController::class, "register"]);
 Route::post("/login", [UserController::class, "login"]);
 Route::group(["middleware" => ["auth:sanctum"]], function() {
     Route::post("/logout", [UserController::class, "logout"]);
     Route::apiResource("/favourite", FavouriteController::class);
 });
+
+
