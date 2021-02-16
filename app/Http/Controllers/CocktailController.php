bcad08785437b18160f97040013f09bf4452b394
CocoBloom <virag.baranyai2@gmail.com>
2021-02-16 12:09:46 +0100

    change getAll() to  getAllCocktails()

--- app/Http/Controllers/CocktailController.php
+++ app/Http/Controllers/CocktailController.php
@@ -4,8 +4,35 @@ namespace App\Http\Controllers;
 
 class CocktailController extends Controller
 {
-    public function getAll() {
-        $redis = app()->make('redis');
-        return $redis->get("cocktails");
+    private $BASE_COCKTAIL_API_URL = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?f=';
+    private $startAsciiIndex = 97;
+    private $lastAsciiIndex = 122;
+    /**
+     * @return array|false|string
+     */
+    public function getAllCocktails()
+    {
+        $allCocktails = array();
+
+        for($i = $this->startAsciiIndex; $i <= $this->lastAsciiIndex; $i++) {
+            $cocktails = $this->getCocktailsByLetter($i)["drinks"];
+            if ($cocktails !== null) {
+                foreach($cocktails as $cocktail) {
+                    array_push($allCocktails, $cocktail);
+                }
+            }
+        }
+        return json_encode(array("cocktails" => $allCocktails, "time" => date("H:i:s")));
+    }
+
+    /**
+     * @param int $letterIndex
+     * @return array
+     */
+    private function getCocktailsByLetter(int $letterIndex) : array
+    {
+        $url = $this->BASE_COCKTAIL_API_URL.chr($letterIndex);
+        $cocktailsByLetter = file_get_contents($url);
+        return json_decode($cocktailsByLetter, true);
     }
 }
