b2c8b77c887fae59af9b875abe234e03c49bc8b9
CocoBloom <virag.baranyai2@gmail.com>
2021-02-16 12:10:18 +0100

    change method on get-all-cocktails endpoint

--- routes/api.php
+++ routes/api.php
@@ -21,7 +21,7 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get("/get-all-cocktails", [CocktailController::class, "getAll"]);
+Route::get("/get-all-cocktails", [CocktailController::class, "getAllCocktails"]);
 Route::post("/register", [UserController::class, "register"]);
 Route::post("/login", [UserController::class, "login"]);
 Route::group(["middleware" => ["auth:sanctum"]], function() {
