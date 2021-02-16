c41356b64b1b701c5b5271918d405f21f631a892
CocoBloom <virag.baranyai2@gmail.com>
2021-02-16 12:55:39 +0100

    switch back to using Redis

--- app/Http/Controllers/CocktailController.php
+++ app/Http/Controllers/CocktailController.php
@@ -4,34 +4,8 @@ namespace App\Http\Controllers;
 
 class CocktailController extends Controller
 {
-    private $BASE_COCKTAIL_API_URL = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?f=';
-    private $startIndexAsc = 97;
-    private $endIndexAsc = 122;
-
-    /**
-     * @return array|false|string
-     */
-    public function getAllCocktails()
-    {
-        $allCocktails = array();
-        for($i = $this->startIndexAsc; $i <= $this->endIndexAsc; $i++) {
-            $cocktails = $this->getCocktailsByLetter($i)["drinks"];
-            if ($cocktails !== null) {
-                foreach($cocktails as $cocktail) {
-                    array_push($allCocktails, $cocktail);
-                }
-            }
-        }
-        return json_encode(array("cocktails" => $allCocktails, "time" => date("H:i:s")));
-    }
-
-    /**
-     * @param int $letterIndex
-     * @return array
-     */
-    private function getCocktailsByLetter(int $letterIndex) : array
-    {
-        $cocktailsByLetter = file_get_contents($this->BASE_COCKTAIL_API_URL.chr($letterIndex));
-        return json_decode($cocktailsByLetter, true);
+    public function getAll() {
+        $redis = app()->make('redis');
+        return $redis->get("cocktails");
     }
 }
