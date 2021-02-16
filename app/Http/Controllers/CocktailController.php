1c65c8b1d9b2f4f7859640130b49bd95f4e33ff5
CocoBloom <virag.baranyai2@gmail.com>
2021-02-16 12:51:13 +0100

    refactor Cocktailcontroller.php

--- app/Http/Controllers/CocktailController.php
+++ app/Http/Controllers/CocktailController.php
@@ -5,16 +5,16 @@ namespace App\Http\Controllers;
 class CocktailController extends Controller
 {
     private $BASE_COCKTAIL_API_URL = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?f=';
-    private $startAsciiIndex = 97;
-    private $lastAsciiIndex = 122;
+    private $startIndexAsc = 97;
+    private $endIndexAsc = 122;
+
     /**
      * @return array|false|string
      */
     public function getAllCocktails()
     {
         $allCocktails = array();
-
-        for($i = $this->startAsciiIndex; $i <= $this->lastAsciiIndex; $i++) {
+        for($i = $this->startIndexAsc; $i <= $this->endIndexAsc; $i++) {
             $cocktails = $this->getCocktailsByLetter($i)["drinks"];
             if ($cocktails !== null) {
                 foreach($cocktails as $cocktail) {
@@ -31,8 +31,7 @@ class CocktailController extends Controller
      */
     private function getCocktailsByLetter(int $letterIndex) : array
     {
-        $url = $this->BASE_COCKTAIL_API_URL.chr($letterIndex);
-        $cocktailsByLetter = file_get_contents($url);
+        $cocktailsByLetter = file_get_contents($this->BASE_COCKTAIL_API_URL.chr($letterIndex));
         return json_decode($cocktailsByLetter, true);
     }
 }
