01e1f36fba1a05c3786235bc8a654deca3866e1d
CocoBloom <virag.baranyai2@gmail.com>
2021-02-23 15:12:55 +0100

    add getAllIngredients()

--- app/Http/Controllers/CocktailController.php
+++ app/Http/Controllers/CocktailController.php
@@ -4,8 +4,13 @@ namespace App\Http\Controllers;
 
 class CocktailController extends Controller
 {
-    public function getAll() {
+    public function getAllCocktails() {
         $redis = app()->make('redis');
         return $redis->get("cocktails");
+    }
+
+    public function getAllIngredients() {
+        $redis = app()->make('redis');
+        return $redis->get("ingredients");
     }
 }
