5ce487067369a574e97216650aa245194eaf65e0
CocoBloom <virag.baranyai2@gmail.com>
2021-02-24 17:09:43 +0100

    continue saveOwnCocktail()

--- app/Http/Controllers/OwnCocktailController.php
+++ app/Http/Controllers/OwnCocktailController.php
@@ -2,12 +2,44 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\OwnCocktail;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
 
 class OwnCocktailController extends Controller
 {
     //
-    public function saveOwnCocktail() {
+    public function saveOwnCocktail(Request $request) {
+
+        try {
+            $validator = Validator::make($request->all(), [
+                "name" => "required",
+                "description" => "required",
+                "ingredients" => "required"
+            ]);
+
+            if ($validator->fails()) {
+                return response()->json([
+                    "message" => "Bad request",
+                    "validation_errors" => $validator->errors()
+                ], 400);
+            }
+
+            $ownCocktailData = $request->all();
+            $ownCocktailData["user_id"] = Auth::id();
+
+            $ownCocktail = OwnCocktail::query()->create($ownCocktailData);
+            $ownCocktailID = $ownCocktail->id;
+            error_log($ownCocktailID);
+//            $ingredients = $ownCocktailData['ingredients'];
+
+
+        } catch (\Exception $e) {
+            error_log($e->getMessage());
+        }
+
+
 
     }
 }
